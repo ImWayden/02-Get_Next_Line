28fb8d0f519cab6c5ed603c6649488ba40927e61
Theo Rodriguez <therodri@e3r9p19.clusters.42paris.fr>
2022-12-07 22:19:59 +0100

    this is done calloc is protected read too i hope i'll get chill corectors

---
+++ get_next_line_bonus.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: therodri <therodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 07:48:32 by therodri          #+#    #+#             */
+/*   Updated: 2022/12/07 22:13:38 by therodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+/*
+# ifdef BUFFER_SIZE
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
+*/
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 100
+# endif
+
+char		*get_next_line(int fd);
+void		*ft_calloc(size_t nmemb, size_t size);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+
+typedef struct s_line
+{
+	char	*line;
+	int		end;
+	int		i;
+	int		len;
+}			t_line;
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: therodri <therodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 05:17:28 by therodri          #+#    #+#             */
+/*   Updated: 2022/12/07 21:44:32 by therodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (n-- > 0)
+		*(ptr++) = (char)c;
+	return (ptr);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	int		true_size;
+
+	true_size = nmemb * size;
+	if (nmemb && true_size / nmemb != size)
+		return (NULL);
+	ptr = malloc(true_size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, true_size);
+	return (ptr);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*pdest;
+	char	*psrc;
+
+	if (!dest || !src)
+		return (NULL);
+	pdest = (char *)dest;
+	psrc = (char *)src;
+	if (pdest < psrc)
+		while (n-- > 0)
+			*(pdest++) = *(psrc++);
+	else
+		while (n-- > 0)
+			pdest[n] = psrc[n];
+	return (dest);
+}
